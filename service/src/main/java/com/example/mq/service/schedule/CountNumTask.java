66b5120bfee7ae4390c545d2b04608b8feea4b64
Qiang.Ma7 <maqianghb@126.com>
2018-11-23 11:12:42 +0800

    调整测试数据

--- service/src/main/java/com/example/mq/service/schedule/CountNumTask.java
+++ service/src/main/java/com/example/mq/service/schedule/CountNumTask.java
@@ -1,11 +1,12 @@
 package com.example.mq.service.schedule;
 
+import com.example.mq.data.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -20,25 +21,26 @@ public class CountNumTask {
 	private static final Logger LOG = LoggerFactory.getLogger(CountNumTask.class);
 
 	@Scheduled(cron = "${count.num.schedule.cron}")
-	public void executeSchedue(){
-		Long startTime =System.currentTimeMillis();
-		SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
-		LOG.info("开始执行定时任务，startTime:{}", sdf.format(new Date(startTime)));
+	public void executeSchedue() {
+		Long startTime = System.currentTimeMillis();
+		LOG.info("开始执行定时任务，startTime:{}", DateUtil.formatDateTime(new Date(startTime)));
 		try {
 			this.doExecute();
 		} catch (Exception e) {
 			LOG.error("定时任务执行出错！", e);
 		}
-		LOG.debug("------debug, 定时任务执行结束，costTime:{}ms", (System.currentTimeMillis()-startTime));
-		LOG.info("------info, 定时任务执行结束，costTime:{}ms", (System.currentTimeMillis()-startTime));
-		LOG.error("------error, 定时任务执行结束，costTime:{}ms", (System.currentTimeMillis()-startTime));
+		LOG.debug("------debug, 定时任务执行结束，costTime:{}ms", (System.currentTimeMillis() - startTime));
+		LOG.info("------info, 定时任务执行结束，costTime:{}ms", (System.currentTimeMillis() - startTime));
+		LOG.error("------error, 定时任务执行结束，costTime:{}ms", (System.currentTimeMillis() - startTime));
 	}
 
-	private void doExecute() throws Exception{
-		int sum =0;
-		for(int i=0;i<100000; i++){
-			sum +=i;
+	private void doExecute() throws Exception {
+		int intSum = 0;
+		long longSum = 0;
+		for (int i = 1; i <= 100 * 10000; i++) {
+			intSum += i;
+			longSum += i;
 		}
-		LOG.info("定时任务计算结果:{}", sum);
+		LOG.info("定时任务计算结果, intSum:{}|longSum:{}", intSum, longSum);
 	}
 }
