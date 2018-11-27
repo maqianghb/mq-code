9675dd5fd5d8e720e5977731cff1f202b87d7d4b
Qiang.Ma7 <maqianghb@126.com>
2018-11-27 10:19:49 +0800

    自定义类加载器-工厂方法

---
+++ service/src/main/java/com/example/mq/service/classLoader/MyLoadClass.java
@@ -0,0 +1,12 @@
+package com.example.mq.service.classLoader;
+
+/**
+ * @program: mq-code
+ * @description: 自定义类加载器的测试加载类接口，接口实现类为自定义加载的类
+ * @author: maqiang
+ * @create: 2018/11/26
+ *
+ */
+public interface MyLoadClass {
+	void testMethod();
+}
