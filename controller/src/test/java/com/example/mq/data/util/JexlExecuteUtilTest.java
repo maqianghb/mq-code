7873a6f820c6f2e36f8267005e2116eb2f03a07b
Qiang.Ma7 <maqianghb@126.com>
2018-12-07 09:05:42 +0800

    日志格式调整

---
+++ controller/src/test/java/com/example/mq/data/util/JexlExecuteUtilTest.java
@@ -0,0 +1,38 @@
+package com.example.mq.data.util;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * @program: mq-code
+ * @description: ${description}
+ * @author: maqiang
+ * @create: 2018/12/6
+ *
+ */
+public class JexlExecuteUtilTest {
+
+	@Test
+	public void checkExps() {
+		String exps ="((a+b)/c+d)*e+f";
+		boolean checkResult =JexlExecuteUtil.checkExps(exps);
+		Assert.assertTrue( checkResult);
+	}
+
+	@Test
+	public void evaluateExps() {
+		String exps ="";
+		Map<String, Object> paramsMap =new HashMap<>(8);
+		paramsMap.put("", 10000);
+		paramsMap.put("", 4000);
+		paramsMap.put("", 0.05);
+		paramsMap.put("", 0.1);
+		Object value =JexlExecuteUtil.evaluateExps(exps, paramsMap);
+	}
+}
