15bd676f82ba819f404414e57d317726c9e1636c
maqiang <maqianghb@126.com>
2024-05-20 22:10:28 +0800

    增加统计数据

--- mqc-wrapper/src/main/java/com/example/mq/wrapper/CommonManager.java
+++ mqc-wrapper/src/main/java/com/example/mq/wrapper/CommonManager.java
@@ -11,12 +11,46 @@ import java.util.List;
 
 public class CommonManager {
 
+    /**
+     * 更新k线和财务数据
+     */
+    @Test
+    public void testUpdateStockData() {
+        LocalDataManager localDataManager =new LocalDataManagerImpl();
+
+        Boolean updateKlineData =true;
+        if(updateKlineData){
+            // 更新本地文件的K线数据
+            localDataManager.queryAndUpdateKLineList();
+
+            // 更新北上持股数据
+            localDataManager.queryAndUpdateNorthHoldShareList();
+        }
+
+        Boolean updateFinanceData =false;
+        if(updateFinanceData){
+            // 更新本地文件的财务数据
+            localDataManager.queryAndUpdateBalanceData();
+            localDataManager.queryAndUpdateIncomeData();
+            localDataManager.queryAndUpdateCashFlowData();
+            localDataManager.queryAndUpdateIndicatorData();
+        }
+
+        // 更新业绩预告和增减持数据
+//        localDataManager.queryAndSaveFinanceNotice();
+//        localDataManager.queryAndSaveHolderIncreaseList();
+    }
+
+
+    /**
+     * 查询并记录指标数据
+     */
     @Test
     public void testQueryAnalysisData(){
         // 参数列表
-        Integer reportYear =2023;
-        FinanceReportTypeEnum reportTypeEnum =FinanceReportTypeEnum.QUARTER_3;
-        String kLineDate ="20240208";
+        Integer reportYear =2024;
+        FinanceReportTypeEnum reportTypeEnum =FinanceReportTypeEnum.QUARTER_1;
+        String kLineDate ="20240520";
 
         LocalDataManager localDataManager =new LocalDataManagerImpl();
         List<String> stockCodeList =localDataManager.getLocalStockCodeList();
@@ -33,12 +67,15 @@ public class CommonManager {
 
     }
 
+    /**
+     * 查询并记录业绩预告数据
+     */
     @Test
     public void testQueryDongChaiData(){
         LocalDataManager localDataManager =new LocalDataManagerImpl();
 
         // 业绩预告数据
-        String reportDate ="2023-12-31";
+        String reportDate ="2024-03-31";
         localDataManager.queryAndSaveFinanceNotice(reportDate);
 
         // by行业预告数据
@@ -47,30 +84,6 @@ public class CommonManager {
         // 股东增减持数据
 //        localDataManager.queryAndSaveHolderIncreaseList();
 
-    }
-
-    @Test
-    public void testUpdateData() {
-        LocalDataManager localDataManager =new LocalDataManagerImpl();
-
-        // 更新本地文件的K线数据
-        localDataManager.queryAndUpdateKLineList();
-
-        // 更新本地文件的财务数据
-        Boolean updateFinanceData =false;
-        if(updateFinanceData){
-            localDataManager.queryAndUpdateBalanceData();
-            localDataManager.queryAndUpdateIncomeData();
-            localDataManager.queryAndUpdateCashFlowData();
-            localDataManager.queryAndUpdateIndicatorData();
-        }
-
-        // 更新北上持股数据
-        localDataManager.queryAndUpdateNorthHoldShareList();
-
-        // 更新业绩预告和增减持数据
-//        localDataManager.queryAndSaveFinanceNotice();
-//        localDataManager.queryAndSaveHolderIncreaseList();
     }
 
 }
